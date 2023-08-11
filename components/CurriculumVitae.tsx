865e63e1dbceaa5c38f6241d207fa650e2bdf9da
Antoine Cau <antoine.cau@les-tilleuls.coop>
2023-08-11 09:32:50 +0200

    first component

---
+++ components/CurriculumVitae.tsx
@@ -0,0 +1,9 @@
+const CurriculumVitae = () => {
+  return (
+    <div className='bg-white rounded-xl w-[21cm] h-[29.7cm]'>
+      <p className='text-black'>Mon CV</p>
+    </div>
+  );
+};
+
+export default CurriculumVitae;
